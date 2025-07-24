a7ccfd604d037e46002fe307477c7b004af52021
VICTOR TAKEYOSHI KIEPER EZAKI <victor.ezaki@alunos.sc.senac.br>
2025-07-24 20:57:56 -0300

    corrigindo o exercício de java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,12 +5,12 @@ public class HelloWorld {
         Scanner scanner = new Scanner(System.in);
 
         double[] notas = {
-                100,
-                50,
-                20,
-                10,
-                5,
-                2
+                100.00,
+                50.00,
+                20.00,
+                10.00,
+                5.00,
+                2.00
         };
 
         double[] moedas = {
@@ -22,41 +22,36 @@ public class HelloWorld {
                 0.01
         };
 
-        String inputValor = "", resto = "";
-        Double valor, aux, result;
+        String inputValor = "";
+        Double valor, aux = 0.00;
 
-        System.out.println("Digite o valor: ");
         inputValor = scanner.next();
 
         valor = Double.parseDouble(inputValor);
 
         System.out.println("\nNotas:");
         for (int i = 0; i < 6; i++) {
-            resto = "0";
             if (valor >= 2) {
                 aux = valor / notas[i];
                 
-                if (aux > 1) {
-                    resto = (Double.toString(aux)).substring(0, 1);
-                    result = Double.parseDouble(resto) * notas[i];
-                    valor -= result;
+                if (aux >= 1) {
+                    aux = Math.round(aux) * notas[i];
+                    valor -= aux;
                 }
             }
-            System.out.println(resto + " notas de R$" + notas[i]);
+            System.out.println(aux + " notas de R$" + String.format("%.2f", notas[i]));
         }
         
         System.out.println("\nMoedas:");
         for (int i = 0; i < 6; i++) {
-            resto = "0";
             aux = valor / moedas[i];
 
             if (aux >= 1) {
-                resto = (Double.toString(aux)).substring(0, 1);
-                result = Double.parseDouble(resto) * moedas[i];
-                valor -= result;
-            }
+                    aux = Math.round(aux) * moedas[i];
+                    valor -= aux;
+                }
 
-            System.out.println(resto + " moedas de R$" + moedas[i]);
+            System.out.println(aux + " moedas de R$" + String.format("%.2f", moedas[i]));
         }
 
         scanner.close();
